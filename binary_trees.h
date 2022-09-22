238b9bce292d6eb5db20462b71919e7c9f08bc64
DammyD <ibukunoluwadawodu@gmail.com>
2022-09-22 08:57:26 -0700

    binary_trees prototypes - updated

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 /**
@@ -46,3 +48,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+#endif
