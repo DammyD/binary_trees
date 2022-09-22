5f415cb430e680cb2623efe617b4725fdd849c8d
DammyD <ibukunoluwadawodu@gmail.com>
2022-09-22 11:22:36 -0700

    Task 2 - updated

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		if (new_node != NULL)
 		{
 			new_node->right = parent->right;
-			new_node->right = NULL;
+			new_node->left = NULL;
 			new_node->parent = parent;
 			new_node->n = value;
 			if (parent->right != NULL)
